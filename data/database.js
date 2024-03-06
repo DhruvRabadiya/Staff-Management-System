c3aaade89f7ace1e9cd8990f5d72a85e3830cba1
Dhruv Rabadiya <dhruvrabdiya1@gmail.com>
2024-03-07 00:30:17 +0530

    final

---
+++ data/database.js
@@ -0,0 +1,28 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let database;
+
+async function connect() {
+  try{
+  const client = await MongoClient.connect("mongodb://localhost:27017");
+  database = client.db("sms");
+  console.log("Connected to the database");
+  } catch (error) {
+    console.error('Error connecting to db:', error.message);
+    throw new Error('Unable to connect to the database');
+  }
+}
+
+
+function getDb() {
+  if (!database) {
+    throw { message: "Database connection not established!" };
+  }
+  return database;
+}
+
+module.exports = {
+  connectToDatabase: connect,
+  getDb: getDb,
+};
